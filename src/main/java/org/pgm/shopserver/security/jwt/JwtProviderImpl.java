3e5608cfe10edbc3e0ef4383d60058045204e345
sifter0926 <sifter0926@gmail.com>
2024-11-21 15:44:36 +0900

    getAuthentication 완료

--- src/main/java/org/pgm/shopserver/security/jwt/JwtProviderImpl.java
+++ src/main/java/org/pgm/shopserver/security/jwt/JwtProviderImpl.java
@@ -1,19 +1,25 @@
 package org.pgm.shopserver.security.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.log4j.Log4j2;
 import org.pgm.shopserver.security.UserPrinciple;
+import org.pgm.shopserver.util.SecurityUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Log4j2
@@ -42,12 +48,53 @@ public class JwtProviderImpl implements JwtProvider {
 
     @Override
     public Authentication getAuthentication(HttpServletRequest request) {
+        Claims claims =extractClaims(request);
+        if(claims==null){
+            return null;
+        }
+        String username = claims.getSubject();
+        Long userId = claims.get("userId", Long.class);
 
-        return null;
+        Set<GrantedAuthority> authorities =
+                Arrays.stream(claims.get("roles").toString().split(","))
+                .map(SecurityUtils::convertToAuthority)
+                .collect(Collectors.toSet());
+
+        UserDetails userDetails=UserPrinciple.builder()
+                .username(username)
+                .authorities(authorities)
+                .id(userId)
+                .build();
+        if(username ==null){
+            log.info("username is null");
+            return null;
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails, null, authorities);
     }
 
     @Override
     public boolean isTokenValid(HttpServletRequest request) {
-        return false;
+        Claims claims = extractClaims(request);
+        if(claims==null){
+            return false;
+        }
+        if(claims.getExpiration().before(new Date())){
+            return false;
+        }
+        return true;
+    }
+
+    private Claims extractClaims(HttpServletRequest request) {
+        String token = SecurityUtils.extractAuthTokenFromRequest(request);
+        if(token == null) {
+            return null;
+        }
+        Key key= Keys.hmacShaKeyFor(JWT_SECRET.getBytes(StandardCharsets.UTF_8));
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
