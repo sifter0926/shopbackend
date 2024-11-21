c3fbf148f8870e8985e723978a9c39064c83644e
sifter0926 <sifter0926@gmail.com>
2024-11-21 11:51:35 +0900

    CustomUserDetailsService 작성

---
+++ src/main/java/org/pgm/shopserver/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package org.pgm.shopserver.security;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.pgm.shopserver.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return null;
+    }
+}
