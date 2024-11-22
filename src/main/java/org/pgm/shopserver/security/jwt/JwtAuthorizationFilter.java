a7b05c50ccfee1f8f8087c1dec8e1d993ca8bf55
miya2009 <miya_2009@naver.com>
2024-11-22 17:01:23 +0900

    JwtAuthorizationFilter 수정

--- src/main/java/org/pgm/shopserver/security/jwt/JwtAuthorizationFilter.java
+++ src/main/java/org/pgm/shopserver/security/jwt/JwtAuthorizationFilter.java
@@ -4,13 +4,16 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
+    @Autowired
     private JwtProvider jwtProvider;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
